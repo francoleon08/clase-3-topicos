452854bd023fb143494919d101fa568bec74211e
francoleon08 <francoleon284@gmail.com>
2024-09-23 18:50:38 -0300

    +

--- fragmento.js
+++ fragmento.js
@@ -2,9 +2,10 @@ class MiComponente123 extends HTMLElement{
     constructor(){
         super();
         this.innerHTML = `
-        <p>BLABLABLABLABLA</p>
+       <p>The Room</p>
         `;
+
     }
 }
 
-customElements.define("team-123", MiComponente123);
+customElements.define("franco-leon", MiComponente123);
