2f48b7f0c3d1d63426cb3f86064c800931e8c641
francoleon08 <francoleon284@gmail.com>
2024-09-23 19:03:49 -0300

    asd

--- fragmento.js
+++ fragmento.js
@@ -1,11 +1,43 @@
-class MiComponente123 extends HTMLElement{
-    constructor(){
+class ListaPeliculas extends HTMLElement {
+    constructor() {
         super();
+        this.peliculas = [
+            "The Room",
+            "Birdemic: Shock and Terror",
+            "Plan 9 from Outer Space",
+            "Manos: The Hands of Fate",
+            "Troll 2"
+        ];
+        this.indiceActual = 0;
+        this.render();
+    }
+
+    render() {
         this.innerHTML = `
-       <p>The Room</p>
+            <div>
+                <button id="anterior">Anterior</button>
+                <span>${this.peliculas[this.indiceActual]}</span>
+                <button id="siguiente">Siguiente</button>
+            </div>
         `;
 
+        this.querySelector('#anterior').addEventListener('click', () => this.mostrarAnterior());
+        this.querySelector('#siguiente').addEventListener('click', () => this.mostrarSiguiente());
+    }
+
+    mostrarAnterior() {
+        if (this.indiceActual > 0) {
+            this.indiceActual--;
+            this.render();
+        }
+    }
+
+    mostrarSiguiente() {
+        if (this.indiceActual < this.peliculas.length - 1) {
+            this.indiceActual++;
+            this.render();
+        }
     }
 }
 
-customElements.define("franco-leon", MiComponente123);
+customElements.define("franco-leon", ListaPeliculas);
